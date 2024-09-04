ab4c692f3d84f5e9ace85c12224550f7feaf9a33
Akshat Bisht <bishtakshat64@gmail.com>
2024-09-04 15:24:41 +0530

    add grid in home

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/moving-border'
 
 const HeroSection = () => {
   return (
-    <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col item-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
+    <div className='dark:bg-black dark:bg-grid-white/[0.09] h-auto md:h-[40rem] w-full rounded-md flex flex-col item-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
          <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
